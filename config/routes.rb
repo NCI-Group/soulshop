8bc4066f3b1d94ca64ed91c0e39ec008718ec8a0
Stephen <stiofan.o.heimhin@gmail.com>
2018-07-25 19:45:43 +0100

    Added main site controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
+  get 'site/home'
+  get 'site/about'
+  get 'site/contact'
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  #The following line needs to be at the bottom of all the routes added. Add any further routes above this
+  root 'site#home'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
