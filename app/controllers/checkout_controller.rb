54774f5ad90230c29d010832e70da4c096a49e40
Stephen <stiofan.o.heimhin@gmail.com>
2018-08-15 18:08:27 +0100

    Added persistence of purchase history.

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -6,6 +6,8 @@ class CheckoutController < ApplicationController
 		
 		totalSouls = 0
 		totalCost = 0
+		
+		purchaseText = "{\"purchase\":["
 	
 		cart.each do |id, quantity|
 			inv = Inventory.find_by_id(id)
@@ -14,13 +16,20 @@ class CheckoutController < ApplicationController
 			end
 			totalSouls += quantity
 			totalCost += (quantity * inv.Cost)
+			purchaseText += "{\"id\":#{id},\"quantity\":#{quantity}},"
 		end
+		
+		purchaseText = purchaseText.chop
+		purchaseText += "]}"
 		
 		session[:cart] = nil
 		
 		if totalSouls == 0 then
 			flash[:notice] = "Cannot check out with nothing in cart."
 		else
+			# Create new Purchase 
+			pur = Purchase.create(:user_id => current_user.id, :contents => purchaseText, :created_at => Time.now, :updated_at => Time.now)
+			pur.save
 			flash[:notice] = "Thank you for your purchase of #{totalSouls} souls for #{totalCost} quarts of blood!"
 		end
 	else
