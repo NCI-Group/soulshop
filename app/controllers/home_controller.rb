ad12059c6ae5083d07c88bdf20061bbc22cccbe4
Anthony Colle <anthonycolle84@gmail.com>
2018-08-15 21:51:07 +0100

    Latest transactions on Home page added

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,6 @@ class HomeController < ApplicationController
   def homepage
 	return if current_user.nil?
 	@prevPurchase = Purchase.where(user_id: current_user.id).last
+  @allProducts = Inventory.all
   end
 end
