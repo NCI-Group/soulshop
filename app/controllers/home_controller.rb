4bdd209b2bc97cd53a450ad1fb9059ccffbacc26
Stephen <stiofan.o.heimhin@gmail.com>
2018-08-15 18:47:35 +0100

    Home now gets previous purchase to display.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
   def homepage
+	return if current_user.nil?
+	@prevPurchase = Purchase.where(user_id: current_user.id).last
   end
 end
--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,7 +2,7 @@ class ProfilesController < ApplicationController
   before_action :set_profile, only: [:show, :edit, :update, :destroy]
 
   before_filter :ensure_admin, :only => [:index, :create]
-  before_filter :valid_id, :only => [:show, :edit, :update, :delete]
+  before_filter :valid_id, :only => [:show, :edit, :update, :destroy]
 
 
 
